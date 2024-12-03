4f83c8a1ea65820f5563b90d498e36334be0a94e
Dipawalihu58nder <respectsuraj67.3@gmail.com>
2024-12-03 08:29:00 +0530

    Add files via upload

---
+++ Btc_Search.py
@@ -0,0 +1,18 @@
+import blocksmith
+address_1 = str(input('Enter the btc address: ')) #'15H9kPoYYM6DLYxjGG9irXtgGcujppgn1w'
+sert=0
+while True:    
+    paddress_1aphrase = blocksmith.KeyGenerator()
+    paddress_1aphrase.seed_input('qwertyuiopasdfghjklzxcvbnm1234567890') # paddress_1aphrase
+    private_Key = paddress_1aphrase.generate_key()
+    address = blocksmith.BitcoinWallet.generate_address(private_Key)
+    sert+=1
+    if address_1 == address:
+        print("we found it ")
+        print("private private_Key = ", private_Key)
+        print("address = ",address)
+        break
+    else:
+        print("trying private private_Key = ", private_Key)
+        print("address = ",address)
+        continue 
